0856a8960531a137453eb392c0fcf87a6cded4b5
MichelleKellyy <michellebrookskelly@gmail.com>
2026-01-12 17:23:13 -0500

    Authcontext + authprovider wrapping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
@@ -10,18 +10,20 @@ import Dashboard from './pages/Dashboard.jsx';
 
 import { auth } from "./firebase/firebase";
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const LoggedIn = localStorage.getItem("loggedIn") === "true";
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={LoggedIn ? <Navigate to="/feed" replace /> : <Home />} />
+        
+        <Route path="/signup" element={LoggedIn ? <Navigate to="/feed" replace /> : <SignUp />} />
+        <Route path="/signin" element={LoggedIn ? <Navigate to="/feed" replace /> : <SignIn />} />
+
+        <Route path="/feed" element={LoggedIn ? <Feed /> : <Navigate to="/" replace /> } />
+        <Route path="/dashboard" element={LoggedIn ? <Dashboard /> : <Navigate to="/" replace /> } />
+
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>

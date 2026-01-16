13b1f93034931477f05b085232b1acf6015d6c6e
Sahil Gupta <sahilgupta70500@gmail.com>
2026-01-16 14:39:48 -0500

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware BEFORE including routes
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://connectkingston.onrender.com"],
+    allow_origins=["https://connectkingston.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
